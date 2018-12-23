697cef34b00078b6ed400c5979af129ed535e744
Cédric David <cedric.david@btopenworld.com>
2018-12-23 15:13:59 +1000

    update files

--- scripts.js
+++ scripts.js
@@ -25,7 +25,7 @@ function flipCard() {
 }
 
 function checkForMatch() {
-  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  let isMatch = firstCard.dataset.pic === secondCard.dataset.pic;
 
   isMatch ? disableCards() : unflipCards();
 }
